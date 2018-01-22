91cda3812dd94eb6ccef6997da03fe37bd3c7a49
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-22 20:11:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObject = Object.assign({[key]}, object);
+  var newObject = Object.assign({}, object, [key]);
   delete newObject.key;
   return newObject;
 }
